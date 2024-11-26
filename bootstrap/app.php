fc2debb07f0013bd9957abb2f87807a110815ca1
Oftal210 <redsunestudio@gmail.com>
2024-11-26 14:37:17 -0500

    cambios en la ruta para el middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\RoleMiddleware;
+use App\Http\Middleware\Authenticate;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +15,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'auth' => Authenticate::class
+        ]);
+        $middleware->alias([
+            'rol' => RoleMiddleware::class
+        ]);
+       
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
